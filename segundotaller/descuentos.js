81d08197cbb999aad4cd510156afd6408fa9b4eb
DEstebanJV <davidjv312@gmail.com>
2021-09-30 16:56:34 -0500

    precio con descuentos

---
+++ segundotaller/descuentos.js
@@ -0,0 +1,7 @@
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
+    return precioConDescuento; 
+}
+
+console.log(calcularPrecioConDescuento(300,50)); 
