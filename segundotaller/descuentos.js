89f9300d2cca56a36cb5df0d73e587eb00e00b34
DEstebanJV <davidjv312@gmail.com>
2021-10-01 13:02:46 -0500

    fin segundo taller

--- segundotaller/descuentos.js
+++ segundotaller/descuentos.js
@@ -1,7 +1,20 @@
-function calcularPrecioConDescuento(precio, descuento){
+function calcularPrecioConDescuento(precio, descuento, cupon){
     const porcentajePrecioConDescuento = 100 - descuento;
+    const porcentajePrecioCupon = 100 - cupon;
     const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
-    return precioConDescuento; 
+    const total = (precioConDescuento * porcentajePrecioCupon)/100;
+    return total;
+    
 }
 
-console.log(calcularPrecioConDescuento(300,50)); 
+/* console.log(calcularPrecioConDescuento(100,10,10)); */
+
+function onClickButtonPrecioDescuento(){
+    const inputPrecio = Number(document.getElementById("inputPrecio").value);
+    const inputDescuento = Number(document.getElementById("inputDescuento").value);
+    const inputCupon = Number(document.getElementById("inputCupon").value)
+    const precioConDescuento = calcularPrecioConDescuento(inputPrecio, inputDescuento, inputCupon);
+    const resultPrecio = document.getElementById("ResultPrecio")   
+    resultPrecio.innerText = "El precio con descuento son: $" + precioConDescuento; 
+    // innertext es para escribir texto en un id de un parrafo
+}
